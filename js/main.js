4b2d882315193a0a4197803dc303b8c6b9f9c782
Iana <ianamashirova@gmail.com>
2022-01-14 19:33:18 +0600

    master

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// 1-задание
+
+// let title = "ddd@bbb@ccc";
+
+// let word = "@@@@@@@@@@@";
+
+// console.log(title.replace("ddd@bbb@ccc", "@@@@@@@@@@@"));
+
+
+// 2-3 задание
+
+// let title = "js";
+
+// let word = "JS";
+
+// console.log(title.replace("js", "JS"));
+// console.log(title.slice());
+
+
+// 4-задание 
+
+// const str = "I am a hero!";
+
+// console.log(str.substr(0, 12));
+
+
+// 5-задание 
+
+let task = prompt('Введите номер карты', 36568745891245);
+
+let title = "36568745891245";
+
+let word = "**********1245";
