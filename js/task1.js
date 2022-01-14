eb7ea31455e817dbdd92197d9e877ff4ccda3287
Iana <ianamashirova@gmail.com>
2022-01-14 19:50:31 +0600

    изменения в task1

--- js/task1.js
+++ js/task1.js
@@ -5,20 +5,3 @@
 // let word = "@@@@@@@@@@@";
 
 // console.log(title.replace("ddd@bbb@ccc", "@@@@@@@@@@@"));
-
-
-
-// 4-задание 
-
-// const str = "I am a hero!";
-
-// console.log(str.substr(0, 12));
-
-
-// 5-задание 
-
-let task = prompt('Введите номер карты', 36568745891245);
-
-let title = "36568745891245";
-
-let word = "**********1245";
